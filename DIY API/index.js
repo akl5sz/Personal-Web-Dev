74a8af6af472a84cfb7c5846b48f5b3cf546cebb
akl5sz <akl5sz@virginia.edu>
2024-10-08 21:40:54 -0400

    created a RESTful API and server that interacts with API

--- DIY API/index.js
+++ DIY API/index.js
@@ -68,7 +68,7 @@ app.delete("/jokes/:id", (req,res) => {
   const id = parseInt(req.params.id);
   const jokeIndex = jokes.findIndex((joke) => joke.id === id);
   if(jokeIndex >= 0){
-    jokes.slice(jokeIndex,1);
+    jokes.splice(jokeIndex,1);
     res.sendStatus(200);
   } else {
     res.status(404).json({error: `Joke with id: ${id} not found. No jokes were deleted.`});
